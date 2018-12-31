5a662465d9e123b94671cf4a5a55751fbe1d9724
Mina Mekhael <mina.mekhael@ymail.com>
2018-12-31 01:02:28 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ num1/num2
+ end
+
+def assign_variable(value ="Bob")
+  puts "#{value}"
+  "Bob"
 end
 
-def assign_variable(value)
-  
+def argue(phrase= "I'm right and you are wrong!")
+  puts "#{phrase}"
+  "I'm right and you are wrong!"
 end
 
-def argue
-  
+def greeting(greeting, name)
+  puts "Hello!"
 end
 
-def greeting
-
+def return_a_value(phrase= "Nice")
+  puts #{phrase}
+  "Nice"
 end
 
-def return_a_value
-
+def last_evaluated_value(answer= "expert")
+  puts #{answer}
+  "expert"
 end
 
-def last_evaluated_value
-
-end
-
-def pizza_party
-
+def pizza_party(default= "cheese")
+  puts #{default}
+  default
 end
